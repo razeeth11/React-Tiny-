879f95f0f00c4bf38d433347a36cd5d1ebba176d
razeeth11 <razeeth011@gmail.com>
2023-03-16 12:57:24 +0530

    Data from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App(){
@@ -11,58 +12,18 @@ function App(){
 
 function Phone() {
 
- const data =  [
-    {
-      "model": "OnePlus 9 5G",
-      "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-      "company": "Oneplus"
-    },
-    {
-      "model": "Iphone 13 mini",
-      "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-      "company": "Apple"
-    },
-    {
-      "model": "Samsung s21 ultra",
-      "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-      "company": "Samsung"
-    },
-    {
-      "model": "Xiomi mi 11",
-      "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-      "company": "Xiomi"
-    },
-    {
-      "model": "OnePlus 9 5G",
-      "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-      "company": "Oneplus"
-    },
-    {
-      "model": "Iphone 13 mini",
-      "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-      "company": "Apple"
-    },
-    {
-      "model": "Samsung s21 ultra",
-      "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-      "company": "Samsung"
-    },
-    {
-      "model": "Xiomi mi 11",
-      "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-      "company": "Xiomi"
-    },
-    {
-      "model": "Samsung s21 ultra",
-      "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-      "company": "Samsung"
-    },
-  ]
+  const [mobData,setData] = useState([])
 
+ useEffect(()=>{
+  fetch("https://react-tiny-node-js.vercel.app/data")
+  .then((res)=> res.json())
+  .then((det)=> setData(det))
+ },[])
+   
   return (
 
     <div className="phone-list-container">
-       {data.map((mv,index)=> <Comp key={index} model={mv.model} image={mv.img} company={mv.company} />)}
+       {mobData.map((mv,index)=> <Comp key={index} model={mv.model} image={mv.img} company={mv.company} />)}
     </div>
   )
 }
@@ -77,6 +38,8 @@ function Comp({model,image,company}){
     </div>
   )
 }
+
+
 
 
 export default App
