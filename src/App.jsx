a0408f9fe2360876206436e69721fbbc39985cf7
razeeth11 <razeeth011@gmail.com>
2023-03-17 09:10:21 +0530

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
 import { useState,useEffect } from 'react'
 import './App.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App(){
   return(
     <div className="App">
-      <Phone/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signUp" element={<SignUP />} />
+        <Route path="/mobile" element={ <Protected> <Phone/> </Protected>} />
+      </Routes>
     </div>
   )
 }
+
+function Protected({children}){
+
+  const token = true;
+  return(
+    <div>
+      {token ? children : "Unauthorized entry!"}
+    </div>
+  )
+}
+
+function Home(){
+  return(
+    <div className="home">
+      <h1>Welcome to the mobile app</h1>
+    </div>
+  )
+}
+ 
+function SignUP(){
+
+  const style = {
+    marginTop : "10px",
+  }
+  return(
+    <form>
+      <div className="container">
+      <TextField sx={style} className='input' id="filled-basic" label="Username/Email" variant="filled" />
+      <TextField sx={style} className='input' id="filled-basic" label="Password" variant="filled" />
+      <TextField sx={style} className='input' id="filled-basic" label="Confirm password" variant="filled" />
+
+      <Button sx={style} variant="contained">Submit</Button>
+      </div>
+    </form>
+  )
+}
+
 
 
 function Phone() {
